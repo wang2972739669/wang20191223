9ac07cb8cd08ea9680866e30a02f4995988be1f8
我的电脑 <@DESKTOP-PVO8ES2>
2019-12-23 19:54:54 +0800

    333

--- src/hr/demo/Demo.java
+++ src/hr/demo/Demo.java
@@ -8,8 +8,9 @@ public class Demo {
 		System.out.println("hehe");
 		System.out.println("yyy");	
 		System.out.println("zzz");
-		System.out.println("xxl");
-		System.out.println("222");
+		System.out.println("xyz");
+		System.out.println("111");
+		System.out.println("55555");
 	}
 
 }
