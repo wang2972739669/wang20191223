f5f8d14e50e47f1d0b2a0f948f15d6e81e99019c
我的电脑 <@DESKTOP-PVO8ES2>
2019-12-23 17:07:16 +0800

    第二此提交

--- src/hr/demo/Demo.java
+++ src/hr/demo/Demo.java
@@ -3,8 +3,8 @@ package hr.demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		
-		System.out.println("haha");
+		System.out.println("word");
+		System.out.println("haha ");
 	}
 
 }
