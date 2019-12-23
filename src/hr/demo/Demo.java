43c27228f50859d2fe7ca1f94995a6a1f2baeb36
我的电脑 <@DESKTOP-PVO8ES2>
2019-12-23 17:43:21 +0800

    提交本地

--- src/hr/demo/Demo.java
+++ src/hr/demo/Demo.java
@@ -5,7 +5,12 @@ public class Demo {
 		System.out.println("hello");
 		System.out.println("word");
 		System.out.println("haha ");
-		System.out.println("江小皮");		
+		System.out.println("hehe");
+		System.out.println("江小皮");	
+		System.out.println("凡人修仙传");
+		System.out.println("hehe");
+		System.out.println("江小皮");
+		System.out.println("九星毒奶");
 	}
 
 }
